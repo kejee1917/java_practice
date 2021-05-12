248e7ece829741b3f7106952068bd991b11a82b5
kejee1917 <83866279+kejee1917@users.noreply.github.com>
2021-05-12 16:37:27 +0900

    Update SortingSwap.java

--- src/sec07/SortingSwap.java
+++ src/sec07/SortingSwap.java
@@ -37,7 +37,7 @@ public class SortingSwap {
 		}else{
 			for(int i=0; i<num.length-1; i++) {
 				for(int j=i+1; j<num.length; j++) {
-					if(num[i] > num[j]) {
+					if(num[i] < num[j]) {
 						temp = num[i];
 						num[i] = num[j];
 						num[j] = temp;
